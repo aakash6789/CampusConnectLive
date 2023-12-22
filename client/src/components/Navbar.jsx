f486f3396e9844a227434eb9f630d9110a51c1e2
Aakash <aakash2506c@gmail.com>
2023-12-22 16:21:29 +0530

    ok

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import logo from '../assets/campuslogo3.jpg'
 const Navbar = () => {
   return (
-    <div>
-      <h1>CampusConnect is live</h1>
+    <div className="fixed top-[0px] left-[0px] z-4 ">
+    <nav className="h-[63px] bg-black flex-row w-screen">
+    <div className="w-[63px]">
+            <img src={logo}  />
+          </div>
+    </nav>
     </div>
   )
 }
