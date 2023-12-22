ac8445deab05df5183251acb8301016848226e0e
Aakash <aakash2506c@gmail.com>
2023-12-22 12:10:44 +0530

    Full force!!

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{html,js}',
     './components/**/*.{html,js}',
   ],
@@ -26,9 +26,9 @@ export default {
       // 'md':{'max':'767px'},
       // 'sm':{'max':'550px'},
       // 'xsm':{'max':'375px'},
-      xs:"480px",
-      sm:"768px",
-      md:"1060px "
+      // xs:"480px",
+      // sm:"768px",
+      // md:"1060px "
     }
   },
   plugins: [],
