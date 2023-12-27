cac8bd254d8a130c1c5d54b4148ff9864e3dcf5d
Aakash <aakash2506c@gmail.com>
2023-12-27 22:27:38 +0530

    ok

--- client/src/components/MeetingRoom.jsx
+++ client/src/components/MeetingRoom.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import io from 'socket.io-client';
 import { useParams } from 'react-router';
 import useSocket from '../hooks/useSocket';
 const MeetingRoom = () => {
   const {roomId}=useParams();
-    const socket = io('http://localhost:3000');
+
+  const handleUserJoined=(data)=>{
+    const {emailId}=data;
+    console.log("New user joined",emailId);
+  }
+    const {socket} = useSocket();
+    useEffect(()=>{
+   socket.on("user-joined",handleUserJoined);
+    },[socket]);
+
   return (
     <div>
       <h1>Now playing {roomId}</h1>
-      <video id="localVideo" autoplay playsinline controls="false"/>
+      {/* <video id="localVideo" autoplay playsinline controls="false"/> */}
     </div>
   )
 }
