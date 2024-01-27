a226bc4067bdb371be0dd89208a7cbd216eeb6d1
Aakash <aakash2506c@gmail.com>
2024-01-27 13:08:53 +0530

    ok

--- client/src/components/Lobby.jsx
+++ client/src/components/Lobby.jsx
@@ -32,12 +32,12 @@ const handleSubmitForm=useCallback((e)=>{
   
   return (
     <div>
-        <h1 className='w-[50vw] md:mr-[60vw]'>Premium video meetings now free for everyone</h1>
-        <h3 className='md:mr-[30vw] mt-[10vh]'>We re-engineered the service and made a better product that Google meet, teams and Zoom meetings</h3>
+        <h1 className='md:w-[80vw] xs:w-[90vw] text-[3em]'>Premium video meetings now free for everyone</h1>
+        <h3 className=' mt-[10vh] xs:w-[80vw]'>We re-engineered the service and made a better product that Google meet, teams and Zoom meetings</h3>
         <form onSubmit={handleSubmitForm}>
-      <input  type='email' value={email} onChange={e=>setEmail(e.target.value)} className='border-black border-2 px-1 mt-[10px] block' placeholder='Enter email-id'></input>
-      <input type='text' value={room} onChange={e=>setRoom(e.target.value)} className='border-black border-2 px-1 mt-[10px] block' placeholder='Enter code or link'></input>
-      <button className='bg-black text-white mr-[1200px] mt-[20px]' >Enter</button>
+      <input  type='email' value={email} onChange={e=>setEmail(e.target.value)} className='border-black border-2 px-1 xs:mt-[5vh] md:mt-[20vh] block ml-[30vw] xs:ml-[20vw]' placeholder='Enter email-id'></input>
+      <input type='text' value={room} onChange={e=>setRoom(e.target.value)} className='border-black border-2 px-1 mt-[10px] block ml-[30vw] xs:ml-[20vw]' placeholder='Enter code or link'></input>
+      <button className='bg-black text-white  mt-[20px]  xs:mr-[-10vw] ml-[-37vw]' >Enter</button>
       </form>
     </div>
   )
